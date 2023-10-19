b1ac40f1b4c94a43763be6b547217ea70188c18d
Mohammed Sayed <farsfhd123@gmail.com>
2023-10-19 18:09:10 +0300

    Section 17: Working with Forms & User Input (working with BasicForm component

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -33,7 +33,7 @@ const SimpleInput = (props) => {
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
-    if (!enteredNameIsValid || !enteredEmailIsValid) {
+    if (!formIsValid) {
       return;
     }
 
--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,33 +1,85 @@
-import { useState } from "react";
+/******************** Using useState ********************/
+// import { useState } from "react";
+
+// const useInput = (validateValue) => {
+//     const [enteredValue, setEnteredValue] = useState("");
+//     const [isTouched, setIsTouched] = useState(false);
+    
+//     const valueIsValid = validateValue(enteredValue);
+//     const hasError = !valueIsValid && isTouched;
+    
+//     const valueChangeHandler = (event) => {
+//     setEnteredValue(event.target.value);
+// };
+
+// const inputBlurHandler = () => {
+//     setIsTouched(true);
+// };
+
+// const reset = () => {
+//     setEnteredValue("");
+//     setIsTouched(false);
+// };
+
+// return {
+//     value: enteredValue,
+//     isValid: valueIsValid,
+//     hasError,
+//     valueChangeHandler,
+//     inputBlurHandler,
+//     reset,
+// };
+// };
+
+// export default useInput;
+
+/******************** Using useReducer ********************/
+import { useReducer } from "react";
+
+const initialInputState = {
+    value: "",
+    isTouched: false
+}
+
+const inputStateReducer = (state, action) => {
+    if(action.type === "INPUT") {
+        return {value: action.value, isTouched: state.isTouched}
+    }
+    if(action.type === "BLUR") {
+        return {isTouched: true, value: state.value}
+    }
+    if(action.type === "RESET") {
+        return {isTouched: false, value: ""}
+    }
+    return initialInputState;
+}
 
 const useInput = (validateValue) => {
-  const [enteredValue, setEnteredValue] = useState("");
-  const [isTouched, setIsTouched] = useState(false);
+    const [inputState, dispatch] = useReducer(inputStateReducer, initialInputState);
 
-  const valueIsValid = validateValue(enteredValue);
-  const hasError = !valueIsValid && isTouched;
+    const valueIsValid = validateValue(inputState.value);
+    const hasError = !valueIsValid && inputState.isTouched;
 
-  const valueChangeHandler = (event) => {
-    setEnteredValue(event.target.value);
-  };
+    const valueChangeHandler = (event) => {
+        dispatch({type: "INPUT", value: event.target.value})
+    }
 
-  const inputBlurHandler = () => {
-    setIsTouched(true);
-  };
+    const inputBlurHandler = () => {
+        dispatch({type: "BLUR"})
+    }
 
-  const reset = () => {
-    setEnteredValue("");
-    setIsTouched(false);
-  };
+    const reset = () => {
+        dispatch({type: "RESET"})
+    }
 
-  return {
-    value: enteredValue,
-    isValid: valueIsValid,
-    hasError,
-    valueChangeHandler,
-    inputBlurHandler,
-    reset,
-  };
-};
+    return {
+        value: inputState.value,
+        isValid: valueIsValid,
+        hasError,
+        valueChangeHandler,
+        inputBlurHandler,
+        reset
+    }
+}
 
 export default useInput;
